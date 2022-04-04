3493344953a6771b5270c3dd314f8e846799bbab
Quan-p <quanp155@gmail.com>
2022-04-04 16:22:40 -0500

    Import corresponding game images to page

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -1,6 +1,12 @@
+import famous_dogs from '../assets/famous_dogs.jpg'
 const Game1 = () => {
     return (
-        <h1>Game 1</h1>
+        <>
+            <h1>Game 1</h1>
+            <div className='img-container'>
+                <img src={famous_dogs} alt='dogs'></img>
+            </div>
+        </>
     )
 }
 
--- src/components/Game2.js
+++ src/components/Game2.js
@@ -1,6 +1,14 @@
+import AD from '../assets/AD2.222.jpg'
+
 const Game2 = () => {
     return (
-        <h1>Game 2</h1>
+        <>
+            <h1>Game 2</h1>
+            <div className='img-container'>
+                <img src={AD} alt='artwork'></img>
+            </div>
+            
+        </>
     )
 }
 
