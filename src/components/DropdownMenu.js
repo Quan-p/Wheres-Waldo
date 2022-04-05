e12c87f7f9bfa7fd9e4e309ee8b880a204d70418
Quan-p <quanp155@gmail.com>
2022-04-05 16:03:23 -0500

    Style dropdown menu

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './DropdownMenu.styles.scss'
 
 const DropdownMenu = (props) => {
     const charLocation = props.charLocation;
@@ -10,9 +11,27 @@ const DropdownMenu = (props) => {
     menuLocation['--dropdown-left'] = xPos + 20 + 'px';
     menuLocation['--dropdown-top'] = yPos + 90 + 'px';
 
+    const checkLocation;
 
     return (
-        <div>h</div>
+        <div>
+            {/* <div className='dropdown'>
+                <div className='table'>
+                    {props.characterState.map((char) => {
+                        return !char.found ? (
+                            <div
+                                key={char.name}
+                                className='row'
+                                onClick={() => checkLocation(char.name)}
+                            >
+                                <img/>
+                                <p>{char.name}</p>
+                            </div>
+                        ) : (false);
+                    })}
+                </div>
+            </div> */}
+        </div>
     );
 };
     
