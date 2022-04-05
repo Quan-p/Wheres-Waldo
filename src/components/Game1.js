68a8f8f3e8d548307f9595ee00f26ee418e4674a
Quan-p <quanp155@gmail.com>
2022-04-05 16:26:36 -0500

    Write handle click function to log mouse click location

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -1,33 +1,48 @@
+import { useState } from 'react';
 import famous_dogs from '../assets/famous_dogs.jpg';
 import CopperHound from '../assets/characters/CopperHound.png';
 import Max from '../assets/characters/Max.png';
 import Snuffles from '../assets/characters/Snuffles.png';
 import './game.styles.scss'
+const charState = [
+    {
+        name: 'Copper',
+        found: false,
+        image: CopperHound,
+    },
+    {
+        name: 'Max',
+        found: false,
+        image: Max,
+    },
+    {
+        name: 'Snuffles',
+        found: false,
+        image: Snuffles,
+    },
+];
+
+let boardSize;
 
 const Game1 = () => {
-    const charState = [
-        {
-            name: 'Copper',
-            found: false,
-            image: CopperHound,
-        },
-        {
-            name: 'Max',
-            found: false,
-            image: Max,
-        },
-        {
-            name: 'Snuffles',
-            found: false,
-            image: Snuffles,
-        },
-    ];
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [characterState, setCharacterState] = useState(charState);
+    const [userCoords, setUserCoords] = useState([]);
+
+    const handleClick = (e) => {
+        boardSize = e.target.getBoundingClientRect();
+        let x = e.clientX - boardSize.left;
+        let y = e.clientY - boardSize.top;
+        setUserCoords([x, y]);
+        console.log(x, y);
+        showDropdown ? setShowDropdown(false) : setShowDropdown(true);
+    }
 
     return (
         <>
             <h1>Game 1</h1>
             <div className='img-container'>
-                <img src={famous_dogs} alt='dogs'></img>
+                <img src={famous_dogs} alt='dogs' onClick={handleClick} />
             </div>
         </>
     )
