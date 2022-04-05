7236b3f4cee4dffaae6e31673d34cc420ffdc55a
Quan-p <quanp155@gmail.com>
2022-04-05 16:14:44 -0500

    Import 3 dog characters image into object

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -1,8 +1,28 @@
 import famous_dogs from '../assets/famous_dogs.jpg';
+import CopperHound from '../assets/characters/CopperHound.png';
+import Max from '../assets/characters/Max.png';
+import Snuffles from '../assets/characters/Snuffles.png';
 import './game.styles.scss'
 
 const Game1 = () => {
-    
+    const charState = [
+        {
+            name: 'Copper',
+            found: false,
+            image: CopperHound,
+        },
+        {
+            name: 'Max',
+            found: false,
+            image: Max,
+        },
+        {
+            name: 'Snuffles',
+            found: false,
+            image: Snuffles,
+        },
+    ];
+
     return (
         <>
             <h1>Game 1</h1>
