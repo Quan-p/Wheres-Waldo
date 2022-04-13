3b1d5a8e75eb281df486ff848870a18ffd1389ab
Quan-p <quanp155@gmail.com>
2022-04-13 16:36:27 -0500

    Create modal to appear upon winning the game and style to show over everything

--- src/components/GameEnd.js
+++ src/components/GameEnd.js
@@ -1,5 +1,25 @@
-const GameEnd = () => {
-    console.log('gg');
+import React, { useState } from "react";
+import './GameEnd.styles.scss';
+
+const GameEnd = (props) => {
+    const [visibility, setVisibility] = useState('modal-show');
+
+    const changeVis = () => {
+        setVisibility('modal-hidden');
+    };
+
+    return props.showModal ? (
+        <div className={visibility} id="modal">
+            <h2>Modal Window</h2>
+            <div className="content">content</div>
+            <div className="actions">
+                <button className='closeBtn' onClick={changeVis}>
+                    close
+                    
+                </button>
+            </div>
+      </div>
+    ) : (false);
 }
 
 export default GameEnd;
