a65c9e9500c6dd564098e8f13f61314e12af093b
Quan-p <quanp155@gmail.com>
2022-04-14 15:51:37 -0500

    Navbar given sticky class to always be on top

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
 import './Nav.styles.scss'
 const Nav = () => {
+    const [stickyClass, setStickyClass] = useState('')
+
+    useEffect(() => {
+        window.addEventListener('scroll', stickNavbar);
+
+        return () => {
+            window.removeEventListener('scroll', stickNavbar);
+        };
+    }, []);
+
+    const stickNavbar = () => {
+        if(window !== undefined) {
+            let windowHeight = window.scrollY;
+            windowHeight > 100 ? setStickyClass('sticky-nav') : setStickyClass('');
+        }
+    }
+
     return (
-        <>
+        <div className={`nav-container ${stickyClass}`}>
             <nav className='dropdown-nav'>
-                <button className="dropbtn">
-                    <MenuIcon />
-                </button>
+                <div >
+                    <button className="dropbtn">
+                        <MenuIcon />
+                    </button>
+                </div>
+                
                     <div className='dropdown-content'>
                         <div>
                             <Link to="/">Home</Link>
@@ -21,7 +42,7 @@ const Nav = () => {
                     </div>
             </nav>
             <Outlet />
-        </>
+        </div>
     )
 }
 
