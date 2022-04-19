bc7f29b41155c5604e4adf47e5afc392d2dc8971
Quan-p <quanp155@gmail.com>
2022-04-19 14:34:07 -0500

    Adjust close button spacing with modal contents

--- src/components/GameEnd.js
+++ src/components/GameEnd.js
@@ -10,16 +10,18 @@ const GameEnd = (props) => {
 
     return props.showModal ? (
         <div className={visibility} id="modal">
-            <h2>Modal Window</h2>
-            <div className="content">content</div>
             <div className='close-container' onClick={changeVis}>
                 <div className="leftright"></div>
                 <div className="rightleft"></div>
                 <label className="close" >close</label>
             </div>
-            {/* <button className='closeBtn' >
-                close
-            </button> */}
+                
+            <div className='main-modal'>
+                <h2>Modal Window</h2>
+                <div className="content">
+                    Content
+                </div>
+            </div>
             
       </div>
     ) : (false);
