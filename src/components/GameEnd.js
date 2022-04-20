e11560553940afdb66476ba5cd99844b75ef46c7
Quan-p <quanp155@gmail.com>
2022-04-20 14:48:08 -0500

    Move form for name submit to modal at end of game

--- src/components/GameEnd.js
+++ src/components/GameEnd.js
@@ -4,9 +4,15 @@ import './GameEnd.styles.scss';
 
 const GameEnd = (props) => {
     const [visibility, setVisibility] = useState('modal-show');
+    const [playerName, setPlayerName] = useState();
 
     const changeVis = () => {
         setVisibility('modal-hidden');
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(playerName);
     };
 
     return props.showModal ? (
@@ -20,7 +26,17 @@ const GameEnd = (props) => {
             <div className='main-modal'>
                 <h2>Modal Window</h2>
                 <div className="content">
-                    <Highscores />
+                    <div className='highscores'>
+                        <Highscores /> 
+                    </div>
+                    <div>
+                        <form className='score-submit' onSubmit={handleSubmit}>
+                            <p>Your Time: </p>
+                            <h2>Enter Name</h2>
+                            <input type='text' id='name' placeholder='Anonymous' onChange={(e) => setPlayerName(e.target.value)} required></input>
+                            <button type='submit'>SUBMIT</button>
+                        </form>
+                    </div>
                 </div>
             </div>
             
