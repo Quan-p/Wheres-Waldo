28dea0de6bde4e331b6869e180d996a9f90ae7d8
Quan-p <quanp155@gmail.com>
2022-04-21 17:43:53 -0500

    Fix highscore sorting to only deal with seconds and handle leading zeros appropriately

--- src/components/Highscores.js
+++ src/components/Highscores.js
@@ -3,20 +3,15 @@ import { v4 as uuidv4 } from 'uuid';
 import { highScores } from '../firebase';
 
 const Highscores = () => {
-    highScores.sort((a, b) => {
-        let amin = a.min;
-        let bmin = b.min;
-        let asec = a.sec;
-        let bsec = b.sec;
-        let atemp = amin+":"+asec;
-        let btemp = bmin+":"+bsec;
-        return atemp.localeCompare(btemp);
-    });
+    highScores.sort((a, b) => a.sec - b.sec);
     let topTen = highScores.slice(0, 10);
+    
     return (
         <div>
             {topTen.map((player) => (
-                <div key={uuidv4()}>Name: {player.Name} Time: {player.min} Minutes {player.sec} Seconds</div>
+                <div key={uuidv4()}>
+                    Name: {player.Name} Time: {Math.floor(player.sec % 3600 / 60).toString().padStart(2,'0')}:{Math.floor(player.sec % 60).toString().padStart(2,'0')} 
+                </div>
             ))}
         </div>
     )
