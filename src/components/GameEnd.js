972d68df68d7f2880fdcf7822465dcc94c5e962c
Quan-p <quanp155@gmail.com>
2022-04-26 23:16:05 -0500

    Bug fix

--- src/components/GameEnd.js
+++ src/components/GameEnd.js
@@ -39,7 +39,7 @@ const GameEnd = (props) => {
             return characters;
         })
 
-        navigate('/Wheres-Waldo/', { replace: true });
+        navigate('#/Wheres-Waldo/', { replace: true });
     };
 
     return props.showModal ? (
