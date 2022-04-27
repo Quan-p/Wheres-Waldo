523f0df16d9b6819ee3cc8f83c0d9f1534e9347e
Quan-p <quanp155@gmail.com>
2022-04-26 23:08:49 -0500

    Update hashrouter homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import Home from './components/Home'
 import './App.scss';
 import Game1 from './components/Game1';
