e41eb1e6059f5e3fdb7a64b1a7808526c36d3076
Quan-p <quanp155@gmail.com>
2022-04-26 22:41:15 -0500

    404 bug fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Nav from './components/Nav';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename='Wheres-Waldo'>
     <Nav />
       <Routes>
           <Route exact path='/Wheres-Waldo/' element={<Home />} />
